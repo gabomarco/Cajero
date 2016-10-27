bbc9ad29861f042e2e4d96740346ca8b66a18ec1
Alexis Hernández <Alexis Hernndez>
2016-10-27 10:38:53 -0500

    se agregaron metodos  depositarSaldo y mostrarSaldo

--- Cuenta.java
+++ Cuenta.java
@@ -1,9 +1,22 @@
 public class Cuenta{
 
 	int idCuenta;
-	String contraseña;
+	String contrasenia;
 	int saldo;
 	int cantidadDepositar;
 	int cantidadRetirar;
 
+	public int mostrarSaldo ()
+	{
+		System.out.println("El saldo de la cuenta: "+idCuenta + " Es: "+ saldo);
+		return saldo;
+	}
+
+	public int depositarSaldo(int cantidadDepositar)
+	{
+		System.out.println("Ingrese la cantidad que desea depositar");
+		cantidadAuxiliar = cantidadDepositar;
+		saldo = saldo + cantidadAuxiliar;
+		System.out.println("El saldo de la cuenta despues de depositar "+ idCuenta + " Es:" + saldo);
+	}
 }
