d167907be1bdcdf87d927be3b5d18eb6ce575d0f
Gabriel Alatriste Garza <Gabriel Alatriste Garza>
2016-10-27 11:08:18 -0500

    modificamos archivo cuenta y agregamos el constructor Cuenta

--- Cuenta.java
+++ Cuenta.java
@@ -8,18 +8,21 @@ public class Cuenta{
 
 
   
+public Cuenta(int idCuenta, String contrasenia, int saldo){
+	this.idCuenta=idCuenta;
+	this.contrasenia=contrasenia;
+	this.saldo= saldo;
 
+}
 	public int mostrarSaldo ()
 	{
 		System.out.println("El saldo de la cuenta: "+idCuenta + " Es: "+ saldo);
 		return saldo;
 	}
 
-	public int depositarSaldo(int cantidadDepositar)
+	public void depositarSaldo(int cantidadDepositar)
 	{
-		System.out.println("Ingrese la cantidad que desea depositar");
-		cantidadAuxiliar = cantidadDepositar;
-		saldo = saldo + cantidadAuxiliar;
+		saldo = saldo + cantidadDepositar;
 		System.out.println("El saldo de la cuenta despues de depositar "+ idCuenta + " Es:" + saldo);
 	}
 
