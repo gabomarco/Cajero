732b42bd75d197605af665957a9e87c946d8018a
Gabriel Alatriste Garza <Gabriel Alatriste Garza>
2016-10-27 09:40:52 -0500

    Fase inicial

---
+++ Cajero.java
@@ -0,0 +1,10 @@
+public class Cajero {
+
+public static void main (String [] args){
+
+		int opcion;
+		char respuesta;
+		int cantidadAuxiliar;
+		
+	}
+}
---
+++ Cuenta.java
@@ -0,0 +1,9 @@
+public class Cuenta{
+
+	int idCuenta;
+	String contraseña;
+	int saldo;
+	int cantidadDepositar;
+	int cantidadRetirar;
+
+}
